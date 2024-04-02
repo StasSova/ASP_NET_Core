1ae258a0be6bbceae7ee5e53d7b487656ea1915f
Стас Сова <stassova2017@gmail.com>
2024-04-02 12:12:09 +0300

    Добавленны исправления в связанные файлы.

--- 01_Start/01_Start/wwwroot/API.js
+++ 01_Start/01_Start/wwwroot/API.js
@@ -1,16 +1,30 @@
 ﻿import { USER_KEY } from "./Constants";
 
 export default class WeatherService {
-    static async search(city) {
+    static async getCurrentWeather(city) {
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${USER_KEY}&units=metric`;
             const response = await fetch(url);
-            const data = await response.json();
-            if (response.ok) {
-                return data;
-            } else {
-                throw new Error("Failed to fetch weather data.");
+            if (!response.ok) {
+                throw new Error("Failed to fetch current weather data.");
             }
+            const data = await response.json();
+            return data;
+        } catch (ex) {
+            console.error(ex);
+            return null;
+        }
+    }
+
+    static async getHourlyForecast(city) {
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${USER_KEY}&units=metric`;
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error("Failed to fetch hourly forecast data.");
+            }
+            const data = await response.json();
+            return data;
         } catch (ex) {
             console.error(ex);
             return null;
--- 01_Start/01_Start/wwwroot/JavaScript.js
+++ 01_Start/01_Start/wwwroot/JavaScript.js
@@ -1,19 +1,44 @@
-﻿
+﻿import { WeatherService } from "./API";
+
 $(document).ready(async function () {
-    const { WeatherService } = await import("./API");
+    const iconUrls = {
+        "01d": "https://openweathermap.org/img/wn/01d.png",
+        "01n": "https://openweathermap.org/img/wn/01n.png",
+        "02d": "https://openweathermap.org/img/wn/02d.png",
+        "02n": "https://openweathermap.org/img/wn/02n.png",
+        "03d": "https://openweathermap.org/img/wn/03d.png",
+        "03n": "https://openweathermap.org/img/wn/03n.png",
+        "04d": "https://openweathermap.org/img/wn/04d.png",
+        "04n": "https://openweathermap.org/img/wn/04n.png",
+        "09d": "https://openweathermap.org/img/wn/09d.png",
+        "09n": "https://openweathermap.org/img/wn/09n.png",
+        "10d": "https://openweathermap.org/img/wn/10d.png",
+        "10n": "https://openweathermap.org/img/wn/10n.png",
+        "11d": "https://openweathermap.org/img/wn/11d.png",
+        "11n": "https://openweathermap.org/img/wn/11n.png",
+        "13d": "https://openweathermap.org/img/wn/13d.png",
+        "13n": "https://openweathermap.org/img/wn/13n.png",
+        "50d": "https://openweathermap.org/img/wn/50d.png",
+        "50n": "https://openweathermap.org/img/wn/50n.png"
+    };
 
     // Указатели на переменные
     const cur_sect = $("#cur_day_sect");
     const horly_sect = $("#horly_sect");
     const error_sect = $("#error_sect");
+
     const cityTitle = $("#cur_day_sect .sect-title p").first();
+    const dateTitle = $("#cur_day_sect .sect-title p").last();
     const date = $("#cur_day_sect .sect-title p").last();
+    const weatherDescr = $("#cur_day_sect .icon-block p");
     const weatherIcon = $("#cur_day_sect .icon-block img");
     const temperature = $("#temp-value");
     const minTemperature = $("#min-temp");
     const maxTemperature = $("#max-temp");
     const windSpeed = $("#wind-speed");
     const hourlySection = $("#horly_sect .items");
+
+    const weekday = $("#weekday");
 
     // Скрываем все элементы
     cur_sect.hide();
@@ -36,25 +61,60 @@ $(document).ready(async function () {
     $("#search_btn").on("click", async function () {
         var city = $("#search input").val();
         if (city) {
-            var weatherData = await WeatherService.search(city);
-
+            var weatherData = await WeatherService.getCurrentWeather(city);
             if (weatherData) {
-                // Очищаем предыдущие данные о погоде
-                hourlySection.empty();
-                // Показываем секции с информацией о погоде
+                const weatherObject = {
+                    cityName: weatherData.name,
+                    timestamp: new Date(weatherData.dt * 1000).toLocaleDateString(),
+                    currentTemp: weatherData.main.temp,
+                    minTemp: weatherData.main.temp_min,
+                    maxTemp: weatherData.main.temp_max,
+                    weatherIcon: weatherData.weather[0].icon,
+                    weatherDescr: weatherData.weather[0].main,
+                    windSpeed: weatherData.wind.speed
+                };
+                dateTitle.text(weatherObject.timestamp);
+                cityTitle.text(weatherObject.cityName);
+                weatherDescr.text(weatherObject.weatherDescr);
+                weatherIcon.attr("src", iconUrls[weatherObject.weatherIcon]);
+                temperature.text(weatherObject.currentTemp);
+                minTemperature.text(weatherObject.minTemp);
+                maxTemperature.text(weatherObject.maxTemp);
+                windSpeed.text(weatherObject.windSpeed);
+
+
+                const currentDate = new Date();
+                const options = { weekday: 'long' };
+                const formattedDate = currentDate.toLocaleDateString('en-US', options);
+                weekday.text(formattedDate);
                 cur_sect.show();
-                horly_sect.show();
                 error_sect.hide(); // Скрываем блок с ошибкой
-                // Добавляем элементы погоды в коллекцию
-                weatherData.forEach(weatherItem => {
-                    addWeatherItem(
-                        weatherItem.time,
-                        weatherItem.icon,
-                        weatherItem.description,
-                        weatherItem.temperature,
-                        weatherItem.windSpeed
-                    );
-                });
+
+
+                var hourlyData = await WeatherService.getHourlyForecast(city);
+                if (hourlyData) {
+                    console.log(hourlyData);
+                    // Очищаем предыдущие данные о погоде
+                    hourlySection.empty();
+                    // Показываем секции с информацией о погоде
+                    horly_sect.show();
+
+                    // Добавляем элементы погоды в коллекцию
+                    for (let i = 0, j = 0; j < 5; i += 1, j++) {
+                        const weatherItem = hourlyData.list[i];
+
+                        const time = new Date(weatherItem.dt * 1000).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+                        const icon = iconUrls[weatherItem.weather[0].icon];
+                        const description = weatherItem.weather[0].description;
+                        const temperature = weatherItem.main.temp;
+                        const windSpeed = weatherItem.wind.speed;
+
+                        addWeatherItem(time, icon, description, temperature, windSpeed);
+                    }
+                }
+
+
+
             } else {
                 // Показываем блок с ошибкой
                 error_sect.show();
@@ -63,5 +123,6 @@ $(document).ready(async function () {
             // Показываем блок с ошибкой, если поле ввода пустое
             error_sect.show();
         }
+
     });
 });
